8bdd065a564a23761d2e1e68c1c181bd7f4e9f11
kells\KellsPing <kellsping@gmail.com>
2023-12-03 11:30:49 +0800

    Change view.
    - Change from login acticity to register activity.

--- app/src/main/java/com/example/mychatroomiii/LoginActivity.java
+++ app/src/main/java/com/example/mychatroomiii/LoginActivity.java
@@ -2,13 +2,26 @@ package com.example.mychatroomiii;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    TextView etSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        etSignUp = findViewById(R.id.tvSignUp);
+        etSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
     }
 }
