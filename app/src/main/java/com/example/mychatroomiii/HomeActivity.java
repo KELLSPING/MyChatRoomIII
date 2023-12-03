d6f764a14c3e367141639d8afa4999f68cc0af84
kells\KellsPing <kellsping@gmail.com>
2023-12-03 11:11:16 +0800

    Update view.
    - Update login activity.

--- app/src/main/java/com/example/mychatroomiii/HomeActivity.java
+++ app/src/main/java/com/example/mychatroomiii/HomeActivity.java
@@ -2,13 +2,24 @@ package com.example.mychatroomiii;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class HomeActivity extends AppCompatActivity {
+
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        startActivity(new Intent(HomeActivity.this, LoginActivity.class));
+
+//        if (auth.getCurrentUser() == null){
+//            startActivity(new Intent(HomeActivity.this, LoginActivity.class));
+//        }
     }
 }
