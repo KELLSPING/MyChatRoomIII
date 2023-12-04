84f0d1f925b9b2446b2b21344170ae1741cc610e
Kells_P <kellsping@gmail.com>
2023-12-04 08:57:18 +0800

    Build project.

--- app/src/main/java/com/example/mychatroomiii/HomeActivity.java
+++ app/src/main/java/com/example/mychatroomiii/HomeActivity.java
@@ -1,15 +1,15 @@
 package com.example.mychatroomiii;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
