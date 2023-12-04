43199c6ca5849859a0139bc66a5022f360c57f7b
Kells_P <kellsping@gmail.com>
2023-12-04 20:28:06 +0800

    Add function.  - Send image to chat activity.

--- app/src/main/java/com/example/mychatroomiii/ChatActivity.java
+++ app/src/main/java/com/example/mychatroomiii/ChatActivity.java
@@ -4,11 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.imageview.ShapeableImageView;
+import com.squareup.picasso.Picasso;
+
 public class ChatActivity extends AppCompatActivity {
+
+    String receiverUid, receiverName, receiverImage;
+    ShapeableImageView profileImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        receiverUid = getIntent().getStringExtra("receiverUid");
+        receiverName = getIntent().getStringExtra("receiverName");
+        receiverImage = getIntent().getStringExtra("receiverImage");
+
+        profileImage = findViewById(R.id.profile_image);
+
+        Picasso.get().load(receiverImage).into(profileImage);
     }
 }
