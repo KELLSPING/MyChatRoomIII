80d6d058fe61a96d38359cb5c2fccf7641e0ab98
kells\KellsPing <kellsping@gmail.com>
2023-12-04 22:33:59 +0800

    Create layout.  - Create chat activity layout.

--- app/src/main/java/com/example/mychatroomiii/ChatActivity.java
+++ app/src/main/java/com/example/mychatroomiii/ChatActivity.java
@@ -1,11 +1,18 @@
 package com.example.mychatroomiii;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.TextView;
 
 import com.google.android.material.imageview.ShapeableImageView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
 public class ChatActivity extends AppCompatActivity {
@@ -13,11 +20,18 @@ public class ChatActivity extends AppCompatActivity {
     String ReceiverUid, ReceiverName, ReceiverImage;
     ShapeableImageView profileImage;
     TextView tvReceiverName;
+    FirebaseDatabase database;
+    FirebaseAuth auth;
+    public static String sImage;
+    public static String rImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        database = FirebaseDatabase.getInstance();
+        auth = FirebaseAuth.getInstance();
 
         ReceiverUid = getIntent().getStringExtra("receiverUid");
         ReceiverName = getIntent().getStringExtra("receiverName");
@@ -28,5 +42,20 @@ public class ChatActivity extends AppCompatActivity {
 
         Picasso.get().load(ReceiverImage).into(profileImage);
         tvReceiverName.setText(""+ReceiverName);
+
+        DatabaseReference reference = database.getReference().child("user").child(auth.getUid());
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                sImage = snapshot.child("imageUri").getValue().toString();
+                rImage = ReceiverImage;
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
