70d4905e9db6e29dde9dd05691165ce3c993c73b
Kells_P <kellsping@gmail.com>
2023-12-04 20:39:59 +0800

    Add function.  - Send user name to chat activity.

--- app/src/main/java/com/example/mychatroomiii/ChatActivity.java
+++ app/src/main/java/com/example/mychatroomiii/ChatActivity.java
@@ -3,26 +3,30 @@ package com.example.mychatroomiii;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.google.android.material.imageview.ShapeableImageView;
 import com.squareup.picasso.Picasso;
 
 public class ChatActivity extends AppCompatActivity {
 
-    String receiverUid, receiverName, receiverImage;
+    String ReceiverUid, ReceiverName, ReceiverImage;
     ShapeableImageView profileImage;
+    TextView tvReceiverName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
-        receiverUid = getIntent().getStringExtra("receiverUid");
-        receiverName = getIntent().getStringExtra("receiverName");
-        receiverImage = getIntent().getStringExtra("receiverImage");
+        ReceiverUid = getIntent().getStringExtra("receiverUid");
+        ReceiverName = getIntent().getStringExtra("receiverName");
+        ReceiverImage = getIntent().getStringExtra("receiverImage");
 
         profileImage = findViewById(R.id.profile_image);
+        tvReceiverName = findViewById(R.id.tvReceiverName);
 
-        Picasso.get().load(receiverImage).into(profileImage);
+        Picasso.get().load(ReceiverImage).into(profileImage);
+        tvReceiverName.setText(""+ReceiverName);
     }
 }
